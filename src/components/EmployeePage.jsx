808020dad2decdf930a5499f63781c7294e721f4
Zachary Kuchar <zacharyk323@live.com>
2023-07-18 11:12:07 -0400

    Initial wireframe

---
+++ src/components/EmployeePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function EmployeePage() {
+  return (
+    <div className='employee'>
+      <h1>The employee page</h1>
+      <h1>The header</h1>
+      <h1>The display section</h1>
+    </div>
+  )
+}
+
+export default EmployeePage
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='home'>
+      <h1>The Home page</h1>
+      <h1>SearchBar</h1>
+      <h1>EmployeeList</h1>
+      <h1>EmployeeListItem</h1>
+    </div>
+  )
+}
+
+export default HomePage
