c98280873052e4cae1fba61641f657365a4f3eed
Zachary Kuchar <zacharyk323@live.com>
2023-07-18 11:37:05 -0400

    Added employee model

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./components/HomePage";
 import EmployeePage from "./components/EmployeePage";
+import employees from "./models/employees";
 
 function App() {
   return (
---
+++ src/models/employees.js
@@ -0,0 +1,13 @@
+const employees = [
+  {
+    name: "James King",
+  },
+  {
+    name: "Julie Taylor",
+  },
+  {
+    name: "Eugine Lee",
+  },
+];
+
+module.exports = employees;
