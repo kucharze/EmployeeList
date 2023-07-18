48c67cb9ab770f516ac75afddc0b5d3bec79b485
Zachary Kuchar <zacharyk323@live.com>
2023-07-18 15:10:38 -0400

    Removed unnecceary code

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,10 +4,8 @@ import { useContext } from 'react'
 
 function Display(props) {
     let {emp} = useContext(StaffContext)
-    console.log("emp:",emp)
   return (
     <div>
-        <h1>Hello</h1>
       <h2>{emp.name}</h2>
       <h2>{emp.role}</h2>
     </div>
